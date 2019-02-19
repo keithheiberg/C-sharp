c99fd9bd8b35284c3d37e4e0d08c4e0ee2d1c389
keithheiberg <46172311+keithheiberg@users.noreply.github.com>
2019-02-19 16:58:20 -0600

    Add files via upload

---
+++ C_sharp_p117_NEW/C-sharp_p117/MultArg.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_sharp_p117
+{
+    public class MultArg
+    {
+        public static void DoIt(int arg1)
+        // 1. Create a class. In that class, create a void method that outputs an integer. 
+        // Have the method divide the data passed to it by 2.
+        // 2. In the Main() method, instantiate that class.
+        // 3. Have the user enter a number. Call the method on that number. 
+        // Display the output to the screen. It should be the entered number, divided by two.
+        // 4. Create a method with output parameters.
+        // 5. Overload a method.
+        // 6. Declare a class to be static.
+
+        {
+            try
+            {
+                int number = arg1 / 2;
+                Console.WriteLine("The integer " + arg1 + " / 2 = " + number);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        public static void DoIt(int arg1, out int number, int arg2 = 2)
+        {
+            number = 0;
+            try
+            {
+                Console.WriteLine("If you wish, enter a second--optional--integer:");
+                string argEntry2 = Console.ReadLine();
+                if (argEntry2 != "")
+                {
+                    arg2 = Convert.ToInt32(argEntry2);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            try
+            {
+                number = arg1 * arg2;
+                Console.WriteLine("The integer " + arg1 + " * the integer " + arg2 + " = " + number);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ C_sharp_p117_NEW/C-sharp_p117/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_sharp_p117
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. Create a class. In that class, create a void method that outputs an integer. 
+            // Have the method divide the data passed to it by 2.
+            // 2. In the Main() method, instantiate that class.
+            // 3. Have the user enter a number. Call the method on that number. 
+            // Display the output to the screen. It should be the entered number, divided by two.
+            // 4. Create a method with output parameters.
+            // 5. Overload a method.
+            // 6. Declare a class to be static.
+
+            //Online research: out parameter modifier(C# Reference)
+            //•	03 / 05 / 2018
+            //The out keyword causes arguments to be passed by reference. 
+            // It is like the ref keyword, except that ref requires that 
+            // the variable be initialized before it is passed.It is also like the in keyword, 
+            // except that in does not allow the called method to modify the argument value.
+            // To use an out parameter, both the method definition and the calling method 
+            // must explicitly use the out keyword.For example:
+            //C#CopyRun
+            //int initializeInMethod;
+            //OutArgExample(out initializeInMethod);
+            //Console.WriteLine(initializeInMethod);     // value is now 44
+            //void OutArgExample(out int number)
+            //{
+            //    number = 44;
+            //}
+
+
+            MultArg MultArg1 = new MultArg();
+            int arg1 = 0;
+            int arg2 = 0;
+            int number1;
+            try
+            {
+                Console.WriteLine("Please enter an integer:");
+                string argEntry1 = Console.ReadLine();
+                arg1 = Convert.ToInt32(argEntry1);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            number1 = arg1 / 2;
+            if (arg2 == 0)
+            {
+                MultArg.DoIt(arg1, out int number);
+                Console.WriteLine(number);
+
+            }
+            else
+            {
+                MultArg.DoIt(arg1, out int number);
+                Console.WriteLine(number);
+            }
+
+            Console.WriteLine("Thank you. Have a great day!");
+            Console.ReadLine();
+        }
+    }
+}
